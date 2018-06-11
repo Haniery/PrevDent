8201873bd98dc7c1ebd3b0ca2ceccb8e8d403d87
user_arpia05 <haniery01@gmail.com>
2018-06-11 17:41:07 -0300

    Mostrando lista de agendamento

--- src/pages/agendamento/agendamento.ts
+++ src/pages/agendamento/agendamento.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AgendamentoService } from '../../services/domain/agendamento.service';
+import { AgendamentoDTO } from '../../models/agendamento.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 /**
  * Generated class for the AgendamentoPage page.
@@ -16,6 +18,10 @@ import { AgendamentoService } from '../../services/domain/agendamento.service';
 })
 export class AgendamentoPage {
 
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
+  items: AgendamentoDTO[];
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -25,8 +31,11 @@ export class AgendamentoPage {
   ionViewDidLoad() {
     this.agendamentoService.findAll()
     .subscribe(response =>{
-      console.log(response);
-    })
+      this.items = response;
+    },
+    error => {
+      console.log(error);
+    });
   }
-  
+
 }
